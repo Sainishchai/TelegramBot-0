02d85026e7e826293d51baa83fdfe337f94511a9
Sainishchai <89191791+Sainishchai@users.noreply.github.com>
2023-03-08 09:38:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GouthamSER/TelegramBot /TelegramBot
+  git clone https://github.com/Sainishchai/TelegramBot-0 /TelegramBot-0
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TelegramBot
+  git clone $UPSTREAM_REPO /TelegramBot-0
 fi
-cd /TelegramBot
+cd /TelegramBot-0
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜....🍁"
 python3 bot.py
